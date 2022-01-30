07194c3eb78d10c749b7d35b5866a248942e7467
Martin <martinkabe@seznam.cz>
2022-01-30 21:50:41 +0100

    maze algo - started

---
+++ Algoritmizace_a_Programovani/Procviko/Rekurze/Rekurze_Udemy/Backtracking/maze_problem.py
@@ -0,0 +1,16 @@
+class MazeProblem:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    m = [[1,1,1,1,1],
+        [0,1,1,1,1],
+        [0,0,0,0,1],
+        [1,0,1,1,1],
+        [0,0,0,1,1]]
+    
+    maze_problem = MazeProblem(m)
+    
